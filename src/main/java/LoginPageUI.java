c59a6265415b94311172f5636a0908ed721d20d7
Leyla <leyla.hunn@outlook.com>
2019-11-17 18:39:07 +0000

    Login page test not running yet

--- src/main/java/LoginPageUI.java
+++ src/main/java/LoginPageUI.java
@@ -1,5 +1,42 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+
 public class LoginPageUI {
     public static void main(String args[]){
+        ChromeDriver driver = new ChromeDriver();
+
+        try{
+            driver.get("https://demo.applitools.com/hackathon.html");
+            driver.manage().window().maximize();
+
+        } finally{
+            driver.quit();
+        }
+    }
+
+    public static void LoginPage(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement Heading = wait.until(ExpectedConditions.visibilityOf((WebElement) By.className("auth-header")));
+        driver.findElement(By.className("auth-header"));
 
     }
+    public static final TextField Heading = new TextField("Login Form");
+
+    public static final TextField usernameField = new TextField("Username");
+
+    public static final TextField passwordField = new TextField("Password");
+
+    /*public static void login(WebDriver driver){
+        if(driver.getPageSource().contains("Login Form")){
+        }
+
+        driver.getPageSource().contains("Login Form");
+    }*/
+
 }
--- src/test/java/LoginPageUITest.java
+++ src/test/java/LoginPageUITest.java
@@ -1,11 +1,15 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LoginPageUITest {
+public class LoginPageUITest extends LoginPageUI {
     @Test
     public void LoginTest(){
-        Assert.assertTrue(true);
+        Assert.assertEquals("Login Form", true, true);
+       // Assert.assertEquals("Username", true, true);
+       // Assert.assertEquals("Password", true, true);
+        //Assert.assertTrue(driver.getPageSource().contains("Login Form"));
     }
 }
