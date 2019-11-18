a12cb9cfb0bf5bfe852a677c4e6c84c2c594fede
LHunn <leyla.hunn@outlook.com>
2019-11-18 16:48:00 +0000

    login page UI tests

--- src/test/java/LoginPageUITest.java
+++ src/test/java/LoginPageUITest.java
@@ -1,23 +1,56 @@
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LoginPageUITest extends LoginPageUI {
+// Login Page UI Elements Test
+// Open the login page and write assertions to ensure everything looks OK on that page. i.e. add assertions to ensure all the fields, labels and all other items exist.
 
-    @Test
-    public void LoginTest(){
+public class LoginPageUITest extends LoginPageUI {
+    @Before
+        public void SetUp(){
         System.setProperty("webdriver.chrome.driver", "C:/Users/lhunn/IdeaProjects/functional-test-suite/src/main/libs/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
+    }
 
+    @Test
+    public void Heading(){
+        ChromeDriver driver = new ChromeDriver();
         driver.get("https://demo.applitools.com/hackathon.html");
-
         assertTrue(driver.getPageSource().contains("Login Form"));
     }
+    @Test
+    public void toggleButton(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demo.applitools.com/hackathon.html");
+        assertTrue(driver.getPageSource().contains("logo-w"));
+    }
+    @Test
+    public void Username(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demo.applitools.com/hackathon.html");
+        assertTrue(driver.getPageSource().contains("Username"));
+    }
+    @Test
+    public void Password(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://demo.applitools.com/hackathon.html");
+        assertTrue(driver.getPageSource().contains("Password"));
+    }
+    @AfterEach
+    public void setDown(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.quit();
+    }
+    /*@FindBy(id = "[class='auth-header']")
+    private WebElement username;*/
 }
 
 //for the data driven tests:
