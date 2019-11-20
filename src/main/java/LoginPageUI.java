4e804c089c88a00209221f8aedd85031dcfd817b
LHunn <leyla.hunn@outlook.com>
2019-11-20 15:40:46 +0000

    Login UI tests done but could make them more efficient

--- src/main/java/LoginPageUI.java
+++ src/main/java/LoginPageUI.java
@@ -8,12 +8,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.awt.*;
 
 public class LoginPageUI {
+    public static final String LOGINPAGE = "https://demo.applitools.com/hackathon.html";
+    WebDriver driver;
+
+
     public static void main(String args[]){
         System.setProperty("webdriver.chrome.driver", "C:/Users/lhunn/IdeaProjects/functional-test-suite/src/main/libs/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
 
         try{
-            driver.get("https://demo.applitools.com/hackathon.html");
+            driver.get(LOGINPAGE);
             driver.manage().window().maximize();
 
         } finally{
@@ -21,9 +25,15 @@ public class LoginPageUI {
         }
     }
 
-    public static void LoginPage(WebDriver driver){
-
+    public static void Heading(WebDriver driver){
     }
+    public static void toggleButton(WebDriver driver){
+    }
+    public static void Username(WebDriver driver){
+    }
+    public static void Password(WebDriver driver){
+    }
+
     public static final TextField Heading = new TextField("Login Form");
 
     public static final TextField usernameField = new TextField("Username");
--- src/test/java/LoginPageUITest.java
+++ src/test/java/LoginPageUITest.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,44 +17,67 @@ import static org.junit.jupiter.api.Assertions.*;
 // Open the login page and write assertions to ensure everything looks OK on that page. i.e. add assertions to ensure all the fields, labels and all other items exist.
 
 public class LoginPageUITest extends LoginPageUI {
+
     @Before
         public void SetUp(){
         System.setProperty("webdriver.chrome.driver", "C:/Users/lhunn/IdeaProjects/functional-test-suite/src/main/libs/chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
+        driver.get(LOGINPAGE);
+        driver.manage().window().maximize();
     }
 
     @Test
     public void Heading(){
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://demo.applitools.com/hackathon.html");
+        driver.get(LOGINPAGE);
         assertTrue(driver.getPageSource().contains("Login Form"));
+        driver.quit();
     }
     @Test
     public void toggleButton(){
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://demo.applitools.com/hackathon.html");
+        driver.get(LOGINPAGE);
         assertTrue(driver.getPageSource().contains("logo-w"));
+        driver.quit();
     }
     @Test
     public void Username(){
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://demo.applitools.com/hackathon.html");
+        driver.get(LOGINPAGE);
         assertTrue(driver.getPageSource().contains("Username"));
+        driver.quit();
     }
     @Test
     public void Password(){
         ChromeDriver driver = new ChromeDriver();
-        driver.get("https://demo.applitools.com/hackathon.html");
+        driver.get(LOGINPAGE);
         assertTrue(driver.getPageSource().contains("Password"));
-    }
-    @AfterEach
-    public void setDown(){
-        ChromeDriver driver = new ChromeDriver();
         driver.quit();
     }
+    @Test
+    public void LoginButton(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(LOGINPAGE);
+        assertTrue(driver.getPageSource().contains("Log In"));
+        driver.quit();
+    }
+    @Test
+    public void TickBox(){
+        ChromeDriver driver = new ChromeDriver();
+        driver.get(LOGINPAGE);
+        assertTrue(driver.getPageSource().contains("Remember Me"));
+        driver.quit();
+    }
+    @After
+    public void setDown(){
+       // driver.quit();
+    }
+
+
     /*@FindBy(id = "[class='auth-header']")
     private WebElement username;*/
 }
+
 
 //for the data driven tests:
 //WebElement TextboxContent = driver.findElement(By.cssSelector(".auth-header"));
